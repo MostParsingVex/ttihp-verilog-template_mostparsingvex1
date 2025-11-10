03edb9f6b397e466a02ecc5109e83db091c9a98c
root <bender@2741c2d51405.(none)>
2025-11-10 19:35:23 +0000

    try to connect all inputs

--- src/project.v
+++ src/project.v
@@ -22,8 +22,9 @@ module tt_um_mostparsingvex1(
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out[3:0] = ui_in[7:4];
+  assign uio_out[7:4] = ui_in[7:4];
+  assign uio_oe = 8'hff;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
