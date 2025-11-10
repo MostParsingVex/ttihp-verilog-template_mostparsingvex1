0ea976fcea5947d020d17ba6c8eb40dff146226f
root <bender@2741c2d51405.(none)>
2025-11-10 19:44:11 +0000

    cleaner disconnect

--- src/project.v
+++ src/project.v
@@ -22,8 +22,7 @@ module tt_um_mostparsingvex1(
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out[3:0] = ui_in[7:4];
-  assign uio_out[7:4] = ui_in[7:4];
+  assign uio_out = ui_in[7:4];
   assign uio_oe = 8'hff;
 
   // List all unused inputs to prevent warnings
